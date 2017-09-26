c813f9303a46878bf89bb19558cb7428de22bff7
Erin Shaben <eshaben@icloud.com>
2017-09-26 13:19:45 -0600

    initial commit... set up database

---
+++ models.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def drop_table():
+    with sqlite3.connect('hoops.db') as connection:
+        c = connection.cursor()
+        c.execute("""DROP TABLE IF EXISTS hoops;""")
+    return True
+
+def create_db():
+    with sqlite3.connect('hoops.db') as connection:
+        c = connection.cursor()
+        table= """CREATE TABLE hoops(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        outer_diameter INTEGER NOT NULL,
+        tubing_size TEXT NOT NULL
+        );
+        """
+        c.execute(table)
+    return True
+
+if __name__ == '__main__':
+    drop_table()
+    create_db()
